1ca2aef870ecde765dfd6bececaad9fc2ef87f14
Calin Tataru <calintat@gmail.com>
2023-03-30 13:23:03 +0100

    Move app into its own module

--- sd-gui/src/main.rs
+++ sd-gui/src/main.rs
@@ -1,8 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
-use sd_core::language;
-use sd_gui::highlighter;
+use sd_gui::app::App;
 
 fn main() -> Result<(), eframe::Error> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
@@ -13,51 +12,8 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
     eframe::run_native(
-        "My egui App",
+        "sd-visualiser",
         options,
         Box::new(|_cc| Box::<App>::default()),
     )
-}
-
-#[derive(Default)]
-struct App {
-    code: String,
-    // TODO: eventually want to store monoidal representation too
-}
-
-impl App {
-    fn code_ui(&mut self, ui: &mut egui::Ui) {
-        let mut layouter = |ui: &egui::Ui, source: &str, wrap_width: f32| {
-            let mut layout_job = highlighter::highlight(source);
-            layout_job.wrap.max_width = wrap_width;
-            ui.fonts(|f| f.layout_job(layout_job))
-        };
-        ui.add(
-            egui::TextEdit::multiline(&mut self.code)
-                .font(egui::TextStyle::Monospace)
-                .layouter(&mut layouter),
-        );
-    }
-    fn graph_ui(&mut self, ui: &mut egui::Ui) {
-        ui.label(format!(
-            "Parse result: {:?}",
-            language::grammar::parse(&self.code)
-        ));
-    }
-}
-
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("SD visualiser");
-            ui.columns(2, |columns| {
-                egui::ScrollArea::vertical()
-                    .id_source("code")
-                    .show(&mut columns[0], |ui| self.code_ui(ui));
-                egui::ScrollArea::both()
-                    .id_source("graph")
-                    .show(&mut columns[1], |ui| self.graph_ui(ui));
-            })
-        });
-    }
 }
