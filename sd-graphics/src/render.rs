34a2e75d2155c533f1240b6c5360121c49089f2b
Calin Tataru <calintat@gmail.com>
2023-04-04 13:12:49 +0100

    Draw thunks as boxes and operations as outlined circles

--- sd-graphics/src/render.rs
+++ sd-graphics/src/render.rs
@@ -1,4 +1,7 @@
-use epaint::{emath::RectTransform, Color32, CubicBezierShape, Pos2, Shape, Stroke, Vec2};
+use epaint::{
+    emath::RectTransform, vec2, CircleShape, Color32, CubicBezierShape, Pos2, Rect, RectShape,
+    Rounding, Shape, Stroke, Vec2,
+};
 use itertools::Itertools;
 use sd_core::monoidal::{MonoidalGraph, MonoidalOp};
 use thiserror::Error;
@@ -7,6 +10,11 @@ use crate::layout::{layout, LayoutError};
 
 pub const SCALE: f32 = 50.0;
 pub const STROKE_WIDTH: f32 = 1.0;
+
+pub const BOX_SIZE: Vec2 = vec2(20.0, 20.0);
+pub const RADIUS_UNIT: f32 = 2.5;
+pub const RADIUS_COPY: f32 = 5.0;
+pub const RADIUS_OPERATION: f32 = 10.0;
 
 #[derive(Debug, Error)]
 pub enum RenderError {
@@ -78,11 +86,6 @@ pub fn render(
                         y: 2.0 * y as f32 + 1.0,
                     });
 
-                    // Hide dot on identities.
-                    if !matches!(op, MonoidalOp::Copy { copies: 1 }) {
-                        shapes.push(Shape::circle_filled(center, 5.0, Color32::BLACK));
-                    }
-
                     for &x in input_wires {
                         let input = transform(Pos2 {
                             x: x as f32,
@@ -107,6 +110,28 @@ pub fn render(
                             Color32::TRANSPARENT,
                             Stroke::new(STROKE_WIDTH, Color32::BLACK),
                         )));
+                    }
+
+                    match op {
+                        MonoidalOp::Copy { copies } if *copies != 1 => {
+                            shapes.push(Shape::circle_filled(center, RADIUS_COPY, Color32::BLACK))
+                        }
+                        MonoidalOp::Unit => {
+                            shapes.push(Shape::circle_filled(center, RADIUS_UNIT, Color32::BLACK))
+                        }
+                        MonoidalOp::Operation { .. } => shapes.push(Shape::Circle(CircleShape {
+                            center,
+                            radius: RADIUS_OPERATION,
+                            fill: Color32::WHITE,
+                            stroke: Stroke::new(STROKE_WIDTH, Color32::BLACK),
+                        })),
+                        MonoidalOp::Thunk { .. } => shapes.push(Shape::Rect(RectShape {
+                            rect: Rect::from_center_size(center, BOX_SIZE),
+                            rounding: Rounding::none(),
+                            fill: Color32::WHITE,
+                            stroke: Stroke::new(STROKE_WIDTH, Color32::BLACK),
+                        })),
+                        _ => (),
                     }
                 }
             }
