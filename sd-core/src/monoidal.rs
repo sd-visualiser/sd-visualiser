ac215c19bdf0f8dea8146e3c3443bf16f68caaf2
Calin Tataru <calintat@gmail.com>
2023-04-04 11:56:04 +0100

    Stupid implementation of unfolding thunks

--- sd-core/src/monoidal.rs
+++ sd-core/src/monoidal.rs
@@ -20,12 +20,7 @@ pub enum Operation {
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum MonoidalOp {
     Copy { copies: usize },
-    // Tuple {
-    //     inputs: usize,
-    // },
-    // Untuple {
-    //     outputs: usize,
-    // },
+    Unit,
     Operation { inputs: usize, op_name: Operation },
     Thunk { args: usize, body: MonoidalGraph },
     Swap,
@@ -36,6 +31,7 @@ impl MonoidalOp {
     pub fn number_of_inputs(&self) -> usize {
         match self {
             Self::Copy { .. } => 1,
+            Self::Unit => 0,
             Self::Operation { inputs, .. } => *inputs,
             Self::Thunk { args, body } => body.inputs - args,
             Self::Swap => 2,
@@ -46,8 +42,7 @@ impl MonoidalOp {
     pub fn number_of_outputs(&self) -> usize {
         match self {
             MonoidalOp::Copy { copies } => *copies,
-            // MonoidalOp::Tuple { .. } => 1,
-            // MonoidalOp::Untuple { outputs } => *outputs,
+            MonoidalOp::Unit => 1,
             MonoidalOp::Operation { .. } => 1,
             MonoidalOp::Thunk { .. } => 1,
             MonoidalOp::Swap => 2,
@@ -58,3 +53,67 @@ impl MonoidalOp {
 pub const ID: MonoidalOp = MonoidalOp::Copy { copies: 1 };
 
 pub const DELETE: MonoidalOp = MonoidalOp::Copy { copies: 0 };
+
+// Unfolding
+
+impl MonoidalGraph {
+    pub fn unfold(self) -> Self {
+        Self {
+            inputs: self.inputs,
+            slices: self
+                .slices
+                .into_iter()
+                .flat_map(|slice| {
+                    // Turn each operation into a list of slices.
+                    let sss = slice
+                        .ops
+                        .into_iter()
+                        .map(|op| match op {
+                            MonoidalOp::Thunk { args, body } => {
+                                // Unfold the body of the thunk and add an extra slice at the start.
+                                let mut slices = body.slices;
+                                slices.insert(
+                                    0,
+                                    Slice {
+                                        ops: std::iter::repeat(ID)
+                                            .take(body.inputs - args)
+                                            .chain(std::iter::repeat(MonoidalOp::Unit).take(args))
+                                            .collect(),
+                                    },
+                                );
+                                slices
+                            }
+                            _ => vec![Slice { ops: vec![op] }],
+                        })
+                        .collect::<Vec<_>>();
+
+                    let max_height = sss.iter().map(|ss| ss.len()).max().unwrap();
+                    let mut slices = Vec::with_capacity(max_height);
+                    for i in 0..max_height {
+                        slices.push(Slice {
+                            ops: sss
+                                .iter()
+                                .flat_map(|ss| {
+                                    ss.get(i)
+                                        .cloned()
+                                        .unwrap_or_else(|| {
+                                            let n = ss
+                                                .last()
+                                                .unwrap()
+                                                .ops
+                                                .iter()
+                                                .map(MonoidalOp::number_of_outputs)
+                                                .sum();
+                                            Slice { ops: vec![ID; n] }
+                                        })
+                                        .ops
+                                })
+                                .collect(),
+                        });
+                    }
+                    slices
+                })
+                .collect(),
+        }
+    }
+}
