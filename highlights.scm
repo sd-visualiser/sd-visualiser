c8cb1685d2a15ec1bf656d6c22cfadd0053eb594
Alex Rice <alexrice999@hotmail.co.uk>
2023-04-04 15:27:30 +0000

    Reworked syntax to have thunking as a top level operation
    Fixes #4

--- highlights.scm
+++ highlights.scm
@@ -14,16 +14,12 @@
 [
   (Term_ActiveOp_1)
   (Value_PassiveOp_1)
-  (Term_ActiveOp_5)
-  (Value_PassiveOp_5)
+  (Term_ActiveOp_3)
+  (Value_PassiveOp_3)
 ] @punctuation.bracket
 
 [
   (Thunk__dot)
-  (Term_ActiveOp_3)
-  (Value_PassiveOp_3)
   (Term_ActiveOp_2_vec_delimiter)
-  (Term_ActiveOp_4_vec_delimiter)
   (Value_PassiveOp_2_vec_delimiter)
-  (Value_PassiveOp_4_vec_delimiter)
 ] @punctuation.delimiter
--- sd-core/build.rs
+++ sd-core/build.rs
@@ -3,4 +3,8 @@ use std::path::PathBuf;
 fn main() {
     println!("cargo:rerun-if-changed=src/language.rs");
     rust_sitter_tool::build_parsers(&PathBuf::from("src/lib.rs"));
+
+    // Uncomment to write grammar
+    // let strings = rust_sitter_tool::generate_grammars(&PathBuf::from("src/lib.rs"));
+    // std::fs::write("grammar.json", strings.join("\n")).expect("Unable to write file");
 }
