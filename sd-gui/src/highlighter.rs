439f07239e741476559988d8853c8fc0b629fd48
Alex Rice <alexrice999@hotmail.co.uk>
2023-04-05 09:51:55 +0000

    Cache highlighting

--- sd-gui/src/highlighter.rs
+++ sd-gui/src/highlighter.rs
@@ -1,15 +1,48 @@
 use eframe::{
-    egui::TextFormat,
+    egui::{
+        util::cache::{ComputerMut, FrameCache},
+        Context, TextFormat,
+    },
     emath::Align,
     epaint::{text::LayoutJob, Color32, Stroke},
 };
-use sd_core::language;
-use tree_sitter_highlight::{Highlight, HighlightEvent};
+use sd_core::language::{self, highlighter_config};
+use tree_sitter_highlight::{Highlight, HighlightConfiguration, HighlightEvent};
 
-pub(crate) fn highlight(source: &str) -> LayoutJob {
+pub struct Highlighter {
+    config: HighlightConfiguration,
+}
+
+impl ComputerMut<&str, LayoutJob> for Highlighter {
+    fn compute(&mut self, source: &str) -> LayoutJob {
+        highlight(&self.config, source)
+    }
+}
+
+type HighlightCache<'a> = FrameCache<LayoutJob, Highlighter>;
+
+impl Highlighter {
+    pub fn new() -> Self {
+        Highlighter {
+            config: highlighter_config(),
+        }
+    }
+
+    pub fn highlight(ctx: &Context, source: &str) -> LayoutJob {
+        ctx.memory_mut(|mem| mem.caches.cache::<HighlightCache<'_>>().get(source))
+    }
+}
+
+impl Default for Highlighter {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub(crate) fn highlight(config: &HighlightConfiguration, source: &str) -> LayoutJob {
     let mut job = LayoutJob::default();
     let mut highlight: Option<Highlight> = None;
-    for event in language::highlight(source) {
+    for event in language::highlight(config, source) {
         match event {
             HighlightEvent::HighlightStart(h) => highlight = Some(h),
             HighlightEvent::HighlightEnd => highlight = None,
