a4022abfcf785dd8ec8e3d298442a2a988d2c177
Nick Hu <me@nickhu.co.uk>
2023-04-12 17:20:20 +0000

    Move back to upstream nix-cargo-integration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration?rev=93801e6a0b0790f36b062ee1369e3f674bc941a5"; # TODO(@NickHu): change rev when https://github.com/yusdacra/nix-cargo-integration/pull/119 merged
+  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
   inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nci.inputs.parts.follows = "parts";
 
@@ -17,8 +17,16 @@
       perSystem = { config, lib, pkgs, ... }:
         {
           nci = {
-            stdenv = pkgs.clangStdenv; # needed for rust-sitter wasm compatibility
-            projects."sd".relPath = "";
+            projects."sd" =
+              let set-stdenv = {
+                override = old: { stdenv = pkgs.clangStdenv; };
+              };
+              in
+              {
+                relPath = "";
+                depsOverrides = { inherit set-stdenv; };
+                overrides = { inherit set-stdenv; };
+              };
             crates = {
               "sd-core" = { };
               "sd-hyper" = { };
@@ -50,7 +58,6 @@
                   };
                 };
             };
-            export = true;
           };
           devShells.default = lib.mkForce config.nci.outputs."sd".devShell;
           packages.default = config.nci.outputs."sd-gui".packages.release;
