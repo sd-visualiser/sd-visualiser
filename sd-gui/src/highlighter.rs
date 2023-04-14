b6c094a8d6c5ee9efb1172cf5563a22e131a8071
Alex Rice <alexrice999@hotmail.co.uk>
2023-04-14 10:04:26 +0000

    Switch to pest parser

--- sd-gui/src/highlighter.rs
+++ sd-gui/src/highlighter.rs
@@ -1,23 +1,20 @@
 use eframe::{
     egui::{
         util::cache::{ComputerMut, FrameCache},
-        Context, Style, TextFormat,
+        Context, Style,
     },
-    emath::Align,
-    epaint::{text::LayoutJob, Color32, Stroke},
+    epaint::{text::LayoutJob, Color32, FontId},
 };
-use sd_core::language::{self, highlighter_config};
 use tracing::{event, Level};
-use tree_sitter_highlight::{Highlight, HighlightConfiguration, HighlightEvent};
 
 pub struct Highlighter {
-    config: HighlightConfiguration,
+    // config: HighlightConfiguration,
 }
 
 impl ComputerMut<(CodeTheme, &str), LayoutJob> for Highlighter {
     fn compute(&mut self, (theme, source): (CodeTheme, &str)) -> LayoutJob {
         event!(Level::DEBUG, "Highlighting");
-        highlight(&self.config, theme, source)
+        highlight(theme, source)
     }
 }
 
@@ -26,7 +23,7 @@ type HighlightCache<'a> = FrameCache<LayoutJob, Highlighter>;
 impl Highlighter {
     pub fn new() -> Self {
         Highlighter {
-            config: highlighter_config(),
+            // config: highlighter_config(),
         }
     }
 
@@ -45,25 +42,31 @@ impl Default for Highlighter {
     }
 }
 
-fn highlight(config: &HighlightConfiguration, theme: CodeTheme, source: &str) -> LayoutJob {
-    let mut job = LayoutJob::default();
-    let mut highlight: Option<Highlight> = None;
-    for event in language::highlight(config, source) {
-        match event {
-            HighlightEvent::HighlightStart(h) => highlight = Some(h),
-            HighlightEvent::HighlightEnd => highlight = None,
-            HighlightEvent::Source { start, end } => {
-                job.append(
-                    &source[start..end],
-                    0.0,
-                    highlight
-                        .map(|highlight| theme.format_from_highlight(highlight))
-                        .unwrap_or_default(),
-                );
-            }
-        }
-    }
-    job
+fn highlight(theme: CodeTheme, source: &str) -> LayoutJob {
+    // let job = LayoutJob::default();
+    // let mut highlight: Option<Highlight> = None;
+    // for event in language::highlight(config, source) {
+    //     match event {
+    //         HighlightEvent::HighlightStart(h) => highlight = Some(h),
+    //         HighlightEvent::HighlightEnd => highlight = None,
+    //         HighlightEvent::Source { start, end } => {
+    //             job.append(
+    //                 &source[start..end],
+    //                 0.0,
+    //                 highlight
+    //                     .map(|highlight| theme.format_from_highlight(highlight))
+    //                     .unwrap_or_default(),
+    //             );
+    //         }
+    //     }
+    // }
+    // job
+    LayoutJob::simple(
+        source.to_string(),
+        FontId::default(),
+        theme.keyword_color,
+        0.0,
+    )
 }
 
 #[derive(Copy, Clone, Hash, Debug)]
@@ -101,23 +104,23 @@ impl CodeTheme {
         }
     }
 
-    fn format_from_highlight(&self, highlight: Highlight) -> TextFormat {
-        let color = match highlight.0 {
-            0 => self.keyword_color,
-            1 => self.operator_color,
-            2 => self.variable_color,
-            3 | 4 => self.punctuation_color,
-            _ => panic!("Unexpected highlight"),
-        };
+    // fn format_from_highlight(&self, highlight: Highlight) -> TextFormat {
+    //     let color = match highlight.0 {
+    //         0 => self.keyword_color,
+    //         1 => self.operator_color,
+    //         2 => self.variable_color,
+    //         3 | 4 => self.punctuation_color,
+    //         _ => panic!("Unexpected highlight"),
+    //     };
 
-        TextFormat {
-            font_id: Default::default(),
-            color,
-            background: Color32::TRANSPARENT,
-            italics: false,
-            underline: Stroke::NONE,
-            strikethrough: Stroke::NONE,
-            valign: Align::BOTTOM,
-        }
-    }
+    //     TextFormat {
+    //         font_id: Default::default(),
+    //         color,
+    //         background: Color32::TRANSPARENT,
+    //         italics: false,
+    //         underline: Stroke::NONE,
+    //         strikethrough: Stroke::NONE,
+    //         valign: Align::BOTTOM,
+    //     }
+    // }
 }
--- sd-gui/src/parser.rs
+++ sd-gui/src/parser.rs
@@ -4,24 +4,45 @@ use eframe::egui::{
     util::cache::{ComputerMut, FrameCache},
     Context,
 };
-use rust_sitter::errors::ParseError;
-use sd_core::language::{self, grammar::Expr};
+use from_pest::{ConversionError, FromPest, Void};
+use pest::error;
+use pest::Parser as _;
+use sd_core::language::{Expr, Rule, SDParser};
+use thiserror::Error;
 use tracing::debug;
+
+#[derive(Error, Debug, Clone)]
+pub enum ParseError {
+    #[error("Parsing error:\n{0}")]
+    PError(#[from] error::Error<Rule>),
+    #[error("Convertion error:\n{0:?}")]
+    CError(ConversionError<Void>),
+}
+
+impl From<ConversionError<Void>> for ParseError {
+    fn from(e: ConversionError<Void>) -> Self {
+        ParseError::CError(e)
+    }
+}
 
 #[derive(Default)]
 pub struct Parser {}
 
-impl ComputerMut<&str, Arc<Result<Expr, Vec<ParseError>>>> for Parser {
-    fn compute(&mut self, source: &str) -> Arc<Result<Expr, Vec<ParseError>>> {
+impl ComputerMut<&str, Arc<Result<Expr, ParseError>>> for Parser {
+    fn compute(&mut self, source: &str) -> Arc<Result<Expr, ParseError>> {
         debug!("Parsing");
-        Arc::new(language::grammar::parse(source))
+        Arc::new((|| {
+            let mut pairs = SDParser::parse(Rule::program, source)?;
+            let expr = Expr::from_pest(&mut pairs)?;
+            Ok(expr)
+        })())
     }
 }
 
-type ParserCache<'a> = FrameCache<Arc<Result<Expr, Vec<ParseError>>>, Parser>;
+type ParserCache<'a> = FrameCache<Arc<Result<Expr, ParseError>>, Parser>;
 
 impl Parser {
-    pub fn parse(ctx: &Context, source: &str) -> Arc<Result<Expr, Vec<ParseError>>> {
+    pub fn parse(ctx: &Context, source: &str) -> Arc<Result<Expr, ParseError>> {
         ctx.memory_mut(|mem| mem.caches.cache::<ParserCache<'_>>().get(source))
     }
 }
