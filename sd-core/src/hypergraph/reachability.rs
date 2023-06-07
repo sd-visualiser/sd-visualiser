19dadf51f678dd422df1a6e910b5fb897218e595
Nick Hu <me@nickhu.co.uk>
2023-06-07 14:35:28 +0000

    Make HyperGraph reachability its own module, and add reachability on sets

--- sd-core/src/hypergraph/reachability.rs
+++ sd-core/src/hypergraph/reachability.rs
@@ -17,64 +17,143 @@ impl<V, E> Node<V, E> {
             .unique()
     }
 
+    #[inline]
     pub fn forward_reachable(&self) -> impl Iterator<Item = Self> {
         self.forward_reachable_n(usize::MAX)
     }
 
     pub fn forward_reachable_n(&self, depth_limit: usize) -> impl Iterator<Item = Self> {
-        fn boxed_sucessors<V, E>(node: &Node<V, E>) -> Box<dyn Iterator<Item = Node<V, E>> + '_> {
-            Box::new(node.successors())
-        }
         NReachable {
             depth_limit,
             seen: HashSet::default(),
             frontier: [(0, self.clone())].into_iter().collect(),
-            next_nodes: boxed_sucessors,
+            next_nodes: Self::boxed_sucessors,
         }
     }
 
+    #[inline]
     pub fn backward_reachable(&self) -> impl Iterator<Item = Self> {
         self.backward_reachable_n(usize::MAX)
     }
 
     pub fn backward_reachable_n(&self, depth_limit: usize) -> impl Iterator<Item = Self> {
-        fn boxed_predecessors<V, E>(
-            node: &Node<V, E>,
-        ) -> Box<dyn Iterator<Item = Node<V, E>> + '_> {
-            Box::new(node.predecessors())
-        }
         NReachable {
             depth_limit,
             seen: HashSet::default(),
             frontier: [(0, self.clone())].into_iter().collect(),
-            next_nodes: boxed_predecessors,
+            next_nodes: Self::boxed_predecessors,
         }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn bidirectional_reachable(&self) -> HashSet<Self> {
+        self.bidirectional_reachable_n(usize::MAX)
+    }
+
+    #[must_use]
+    pub fn bidirectional_reachable_n(&self, depth_limit: usize) -> HashSet<Self> {
+        let forward: HashSet<_> = self.forward_reachable_n(depth_limit).collect();
+        let backward: HashSet<_> = self.backward_reachable_n(depth_limit).collect();
+        forward.intersection(&backward).cloned().collect()
+    }
+
+    #[inline]
+    fn boxed_sucessors(node: &Node<V, E>) -> Box<dyn Iterator<Item = Node<V, E>> + '_> {
+        Box::new(node.successors())
+    }
+
+    #[inline]
+    fn boxed_predecessors(node: &Node<V, E>) -> Box<dyn Iterator<Item = Node<V, E>> + '_> {
+        Box::new(node.predecessors())
     }
 }
 
 type NextFn<T> = fn(&T) -> Box<dyn Iterator<Item = T> + '_>;
-struct NReachable<V, E, const BUILT: bool> {
+pub struct NReachable<V, E> {
     depth_limit: usize,
-    seen: HashSet<Node<V, E, BUILT>>,
-    frontier: VecDeque<(usize, Node<V, E, BUILT>)>,
-    next_nodes: NextFn<Node<V, E, BUILT>>,
+    seen: HashSet<Node<V, E>>,
+    // invariant: for each (i, node) in frontier, i increases monotonically
+    frontier: VecDeque<(usize, Node<V, E>)>,
+    next_nodes: NextFn<Node<V, E>>,
 }
 
-impl<V, E, const BUILT: bool> Iterator for NReachable<V, E, BUILT> {
-    type Item = Node<V, E, BUILT>;
+impl<V, E> NReachable<V, E> {
+    #[inline]
+    pub fn forward_from(nodes: impl IntoIterator<Item = Node<V, E>>) -> Self {
+        Self::forward_from_n(nodes, usize::MAX)
+    }
+
+    pub fn forward_from_n(nodes: impl IntoIterator<Item = Node<V, E>>, depth_limit: usize) -> Self {
+        Self {
+            depth_limit,
+            seen: HashSet::default(),
+            frontier: nodes.into_iter().map(|node| (0, node)).collect(),
+            next_nodes: Node::boxed_sucessors,
+        }
+    }
+
+    #[inline]
+    pub fn backward_from(nodes: impl IntoIterator<Item = Node<V, E>>) -> Self {
+        Self::backward_from_n(nodes, usize::MAX)
+    }
+
+    pub fn backward_from_n(
+        nodes: impl IntoIterator<Item = Node<V, E>>,
+        depth_limit: usize,
+    ) -> Self {
+        Self {
+            depth_limit,
+            seen: HashSet::default(),
+            frontier: nodes.into_iter().map(|node| (0, node)).collect(),
+            next_nodes: Node::boxed_predecessors,
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn bidirectional_from(
+        nodes: impl IntoIterator<Item = Node<V, E>> + Clone,
+    ) -> HashSet<Node<V, E>> {
+        Self::bidirectional_from_n(nodes, usize::MAX)
+    }
+
+    #[must_use]
+    pub fn bidirectional_from_n(
+        nodes: impl IntoIterator<Item = Node<V, E>> + Clone,
+        depth_limit: usize,
+    ) -> HashSet<Node<V, E>> {
+        let forward: HashSet<_> = Self::forward_from_n(nodes.clone(), depth_limit).collect();
+        let backward: HashSet<_> = Self::backward_from_n(nodes, depth_limit).collect();
+        forward.intersection(&backward).cloned().collect()
+    }
+
+    #[inline]
+    pub fn bump_depth_limit(&mut self) {
+        self.increase_depth_limit(1);
+    }
+
+    pub fn increase_depth_limit(&mut self, n: usize) {
+        self.depth_limit += n;
+    }
+}
+
+impl<V, E> Iterator for NReachable<V, E> {
+    type Item = Node<V, E>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.frontier.pop_front() {
             None => None,
+            Some((d, node)) if d > self.depth_limit => {
+                self.frontier.push_front((d, node));
+                None
+            }
             Some((d, node)) => {
                 self.seen.insert(node.clone());
-                if d < self.depth_limit {
-                    self.frontier.extend(
-                        (self.next_nodes)(&node)
-                            .filter_map(|s| (!self.seen.contains(&s)).then_some((d + 1, s)))
-                            .collect::<Vec<_>>(),
-                    );
-                }
+                self.frontier.extend(
+                    (self.next_nodes)(&node)
+                        .filter_map(|s| (!self.seen.contains(&s)).then_some((d + 1, s))),
+                );
                 Some(node)
             }
         }
