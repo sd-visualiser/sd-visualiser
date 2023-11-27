63cb65b3f70f7b05c9b8bc44c9e9e60bc2028e15
Alex Rice <alexrice999@hotmail.co.uk>
2023-11-27 14:31:10 +0000

    Reword nix/ci: Attempt 1

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   nixConfig = {
-    extra-substituters = [ "https://sd-visualiser.cachix.org" ];
-    extra-trusted-public-keys = [ "sd-visualiser.cachix.org-1:vGc1hqbmMqHo/iWJyOzn9rCZEAn7ebUrFT7Gp4tM700=" ];
+    extra-substituters = ["https://sd-visualiser.cachix.org"];
+    extra-trusted-public-keys = ["sd-visualiser.cachix.org-1:vGc1hqbmMqHo/iWJyOzn9rCZEAn7ebUrFT7Gp4tM700="];
   };
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.parts.url = "github:hercules-ci/flake-parts";
@@ -11,184 +11,64 @@
   inputs.nci.inputs.parts.follows = "parts";
   inputs.nci.inputs.rust-overlay.follows = "rust-overlay";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs =
-    inputs @ { parts
-    , nixpkgs
-    , nci
-    , rust-overlay
-    , ...
-    }:
-    let
-      nciFromPkgs = pkgs: target: extraDepsOverrides: extraOverrides: {
-        # common config across targets
-        projects.sd = {
-          relPath = "";
-          profiles.release = {
-            features = if target != "web" then [ "cbc" ] else null;
-            runTests = target == "linux";
-          };
+  outputs = inputs @ {
+    parts,
+    nixpkgs,
+    nci,
+    rust-overlay,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+        nci.flakeModule
+      ];
+      perSystem = {
+        pkgs,
+        config,
+        system,
+        lib,
+        ...
+      }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
         };
-        crates = {
-          sd-core = {
-            overrides = {
-              add-inputs.overrideAttrs = oldAttrs: rec {
-                nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ (with pkgs; [
-                  cargo-insta
-                ]);
-              };
-            };
-          };
-          sd-graphics = {
-            overrides = {
-              add-inputs.overrideAttrs = oldAttrs: rec {
-                nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ (with pkgs; [
-                  cbc
-                ]);
-              };
-            };
-          };
-          sd-gui =
-            {
-              export = true;
-              renameTo = "sd-gui-${target}";
-              overrides = {
-                add-inputs.overrideAttrs = oldAttrs: rec {
-                  nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ (with pkgs; [
-                    trunk
-                    wasm-bindgen-cli
-                    binaryen
-                  ]);
-                };
-              } // extraOverrides;
-              depsOverrides = extraDepsOverrides;
-            };
+
+        devShells.default = config.nci.outputs.sd-gui.devShell.overrideAttrs (attrs: {
+          packages = (attrs.packages or [ ]) ++ (with pkgs; [
+            (lib.hiPrio rust-bin.nightly.latest.rustfmt)
+            cargo-insta
+            cbc
+            trunk
+          ]);
+        });
+
+        packages = rec {
+          default = linux;
+          linux = config.nci.outputs.sd-gui.packages.release;
+          web = config.nci.outputs.sd-gui.allTargets."wasm32-unknown-unknown".packages.release;
         };
-      };
-    in
-    nixpkgs.lib.recursiveUpdate
-      (nixpkgs.lib.recursiveUpdate
-        (parts.lib.mkFlake { inherit inputs; }
-          {
-            systems = [ "x86_64-linux" ];
-            imports = [ nci.flakeModule ];
-            perSystem = { self', config, lib, pkgs, system, ... }:
-              {
-                _module.args.pkgs = import inputs.nixpkgs {
-                  inherit system;
-                  overlays = [
-                    inputs.rust-overlay.overlays.default
-                  ];
-                  config = { };
-                };
-                nci =
-                  let
-                    add-gtk.overrideAttrs = oldAttrs: {
-                      nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ (with pkgs; [
-                        pkg-config
-                        atk
-                        gdk-pixbuf
-                        gtk3
-                        pango
-                      ]);
-                      XDG_DATA_DIRS = with pkgs; "${gtk3}/share/gsettings-schemas/${gtk3.name}";
-                    };
-                    add-cbc.overrideAttrs = oldAttrs: {
-                      nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ (with pkgs; [
-                        cbc
-                      ]);
-                    };
-                  in
-                  nciFromPkgs pkgs "linux"
-                    {
-                      inherit add-gtk add-cbc;
-                    }
-                    {
-                      inherit add-gtk add-cbc;
-                      egui-wayland = {
-                        runtimeLibs = with pkgs; [
-                          # graphics libraries are loaded at runtime
-                          # see: https://scvalex.net/posts/63/
-                          libGL
-                          libxkbcommon
-                          wayland
-                          xorg.libX11
-                          xorg.libXcursor
-                          xorg.libXi
-                          xorg.libXrandr
-                        ];
-                      };
-                    };
-                devShells.default = config.nci.outputs.sd.devShell.overrideAttrs (oldAttrs: {
-                  packages = (oldAttrs.packages or [ ]) ++ [ (lib.hiPrio pkgs.rust-bin.nightly.latest.rustfmt) ];
-                });
-                packages.default = config.nci.outputs.sd-gui.packages.release;
+
+        nci = {
+          projects.sd = {
+            path = ./.;
+            profiles.release = {runTests = false;};
+          };
+          crates = {
+            sd-gui = {
+              targets."x86_64-unknown-linux-gnu" = {
+                default = true;
               };
-          })
-        # windows
-        (parts.lib.mkFlake
-          {
-            inherit inputs;
-          }
-          {
-            systems = [ "x86_64-linux" ];
-            imports = [ nci.flakeModule ];
-            perSystem = { self', config, lib, pkgs, system, ... }:
-              {
-                nci =
-                  let
-                    cbc = pkgs.pkgsCross.mingwW64.cbc.overrideAttrs (oldAttrs: {
-                      # not marked as compatible with mingwW64, but it is
-                      meta.platforms = lib.platforms.all;
-                    });
-                    windows-cross-compile = {
-                      CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-                      CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc";
-                      CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS = ''
-                        -L native=${pkgs.pkgsCross.mingwW64.windows.pthreads}/lib
-                        -L native=${pkgs.pkgsCross.mingwW64.bzip2}/lib
-                        -L native=${pkgs.pkgsCross.mingwW64.zlib}/lib
-                        -L native=${cbc}/lib
-                      '';
-                    };
-                  in
-                  nciFromPkgs pkgs "windows"
-                    {
-                      inherit windows-cross-compile;
-                    }
-                    {
-                      inherit windows-cross-compile;
-                      windows-add-dlls.overrideAttrs = oldAttrs: {
-                        postInstall = ''
-                          cp ${pkgs.pkgsCross.mingwW64.stdenv.cc.cc}/x86_64-w64-mingw32/lib/{libgcc_s_seh-1.dll,libstdc++-6.dll} $out/bin
-                          cp ${pkgs.pkgsCross.mingwW64.windows.mcfgthreads_pre_gcc_13}/bin/mcfgthread-12.dll $out/bin
-                          cp ${pkgs.pkgsCross.mingwW64.bzip2}/bin/libbz2-1.dll $out/bin
-                          cp ${pkgs.pkgsCross.mingwW64.zlib}/bin/zlib1.dll $out/bin
-                          cp ${cbc}/bin/{libCbc-3.dll,libCbcSolver-3.dll,libCgl-1.dll,libClp-1.dll,libCoinUtils-3.dll,libOsi-1.dll,libOsiClp-1.dll} $out/bin
-                        '';
-                      };
-                    };
-              };
-          }))
-      # web
-      (parts.lib.mkFlake { inherit inputs; } {
-        systems = [ "x86_64-linux" ];
-        imports = [ nci.flakeModule ];
-        perSystem = { self', config, lib, pkgs, system, ... }:
-          {
-            nci = nciFromPkgs pkgs "web"
-              {
-                set-target = {
-                  CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
-                };
-              }
-              {
-                build-with-trunk.overrideAttrs = oldAttrs: {
-                  # add trunk and other dependencies
-                  nativeBuildInputs =
-                    (oldAttrs.nativeBuildInputs or [ ])
-                    ++ (with pkgs; [ trunk wasm-bindgen-cli binaryen ]);
-                  # override build phase to build with trunk instead
+
+              targets."wasm32-unknown-unknown" = {
+                drvConfig.mkDerivation = {
+                  nativeBuildInputs = with pkgs; [ trunk wasm-bindgen-cli binaryen ];
+
                   buildPhase = ''
                     cd sd-gui
                     HOME=$TMPDIR \
@@ -202,6 +82,43 @@
                   dontInstall = true;
                 };
               };
+
+              depsDrvConfig.mkDerivation = {
+                nativeBuildInputs = with pkgs; [
+                  pkg-config
+                ];
+
+                buildInputs = with pkgs; [
+                  libGL
+                  glib
+                  atk
+                  gtk3
+                ];
+              };
+
+              drvConfig = {
+                mkDerivation = {
+                  nativeBuildInputs = with pkgs; [
+                    pkg-config
+                  ];
+
+                  buildInputs = with pkgs; [
+                    libGL
+                    glib
+                    atk
+                    gtk3
+                  ];
+                };
+              };
+
+              runtimeLibs = with pkgs; [
+                libGL
+                gtk3
+                glib
+              ];
+            };
           };
-      });
+        };
+      };
+    };
 }
